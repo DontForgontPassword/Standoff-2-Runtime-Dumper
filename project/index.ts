fcb1de5263b80d783f56a4a754a1554e07a948aa
ExperienceInMyMind <110235001+DontForgontPassword@users.noreply.github.com>
2024-11-17 05:40:05 -0800

    Update index.ts

--- project/index.ts
+++ project/index.ts
@@ -1,46 +1,62 @@
 import "frida-il2cpp-bridge";
-import "fs"
-import { dump_type, il2cpp_load_symbols, Il2cppFunctionManager, image_get_class, IScriptAnnotation } from "./il2cpp.js";
-import { appendFile, appendFileSync, writeFile, writeFileSync } from "fs";
+import {
+  dump_type,
+  il2cpp_load_symbols,
+  il2cpp_functions,
+  image_get_class,
+  IScriptAnnotation,
+} from "./il2cpp.js";
+import { writeFileSync } from "fs";
 
-const ScriptAnnotation: IScriptAnnotation =
-{
-    ScriptMethod: []
-}
+const ScriptAnnotation: IScriptAnnotation = {
+  ScriptMethod: [],
+};
 
 Il2Cpp.perform(() => {
-    const il2cpp = Module.getBaseAddress("libil2cpp.so");
+  const il2cpp = Module.getBaseAddress("libil2cpp.so");
 
-    if (il2cpp == null) {
-        console.log("Failed to get il2cpp base address");
-        return;
-    }
+  if (il2cpp == null) {
+    console.log("Failed to get il2cpp base address");
+    return;
+  }
 
-    
-    il2cpp_load_symbols()
+  il2cpp_load_symbols();
 
-    let result = ""
+  let dump_result = "";
 
-    const GetAssemblies = new NativeFunction(il2cpp.add(0x1EE8F54), 'pointer', []);
-    const Size = GetAssemblies().add(0x4).readInt()
+  const get_assembly = new NativeFunction(il2cpp.add(0x1ee8f54), "pointer", []);
+  const assemblies_size = get_assembly().add(0x4).readInt();
 
-    for (let assemblyIndex = 0; assemblyIndex <= Size; assemblyIndex++) {
-        const assembly = GetAssemblies().readPointer().add(assemblyIndex * 0x8).readPointer(); // image
-        result += `// Image ${assemblyIndex}: ${assembly.readPointer().readPointer().readCString()}\n`
+  for (
+    let assemblyIndex = 0;
+    assemblyIndex <= assemblies_size;
+    assemblyIndex++
+  ) {
+    const assembly = get_assembly()
+      .readPointer()
+      .add(assemblyIndex * 0x8)
+      .readPointer(); // Il2cppImage
+    dump_result += `// Image ${assemblyIndex}: ${assembly
+      .readPointer()
+      .readPointer()
+      .readCString()}\n`;
+  }
 
-    }
+  const AssemblyCSharp = il2cpp_functions.get_assembly(
+    Memory.allocUtf8String("Assembly-CSharp.dll")
+  ); // Il2cppImage
 
-    const AssemblyCSharp = Il2cppFunctionManager.get_assembly(Memory.allocUtf8String("Assembly-CSharp.dll")); // image
+  const KlassCount = 0; // Provide how much classes you need to dump
 
-    for (let index = 0; index < 10; index++) { // 20729
-        let Klass = image_get_class(AssemblyCSharp.readPointer(), index);
+  for (let index = 0; index < KlassCount; index++) {
+    let Klass = image_get_class(AssemblyCSharp.readPointer(), index);
 
-        result += dump_type(ScriptAnnotation, il2cpp, Klass)
-        
-        console.log(index)
-    }
+    dump_result += dump_type(ScriptAnnotation, il2cpp, Klass);
+  }
 
-    writeFileSync("/data/data/com.axlebolt.standoff2/files/script.json", JSON.stringify(ScriptAnnotation))
-    writeFileSync("/data/data/com.axlebolt.standoff2/files/dump.cs", result);
-
+  writeFileSync(
+    "/data/data/com.axlebolt.standoff2/files/script.json",
+    JSON.stringify(ScriptAnnotation)
+  );
+  writeFileSync("/data/data/com.axlebolt.standoff2/files/dump.cs", dump_result);
 });
